a027df789e9506747defe8ce2787ed8564d4573c
Andres <andres.rivas@mercadolibre.com>
2020-09-14 07:30:34 -0300

    Initial Commit

---
+++ JsonPlaceHolderApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JsonPlaceHolderApp
+//
+//  Created by Andres Rivas on 14/09/2020.
+//  Copyright © 2020 Andres Rivas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
